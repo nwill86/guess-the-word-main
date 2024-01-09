456fedbd532ef781c4a1a1750fde92bae03e6eb2
NATASHA WILLIAMS <147554213+nwill86@users.noreply.github.com>
2024-01-09 17:20:50 -0500

    Updated code to accept and validate player guesses.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // ul where guessed letters appear
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 // button to guess a letter
 const guessLetterButton = document.querySelector(".guess");
 // letter input
@@ -16,23 +16,48 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
-  const placeholderLetters = [];
-  for (const letter of word) {
-    console.log(letter);
-   placeholderLetters.push("●");
-  }
-  wordInProgress.innerText = placeholderLetters.join("");
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
 };
 
 placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
-    console.log(guess); 
-    letterInput.value = "";
+    const goodGuess = validateInput(guess);
+
+if (goodGuess) {
+    makeGuess(guess);
+}
+letterInput.value = "";
 });
 
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
 
+    if (input === 0) {
+        message.innerText = "Please enter a letter.";
+    } else if (input.lenth > 1) {
+        message.innerText = "Please enter a single letter.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter. Try again.";
+    }
+};
