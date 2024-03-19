48eada18b3fd04ebd62e4e3bf5ea516d427174e7
NATASHA WILLIAMS <147554213+nwill86@users.noreply.github.com>
2024-03-19 09:59:51 -0400

    Removed comments

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-// ul where guessed letters appear
 const guessedLettersElement = document.querySelector(".guessed-letters");
-// button to guess a letter
 const guessLetterButton = document.querySelector(".guess");
-// letter input
 const letterInput = document.querySelector(".letter");
-// word in progress
 const wordInProgress = document.querySelector(".word-in-progress");
-// remaining guesses
 const remainingGuessesElement = document.querySelector(".remaining");
-// remaining guesses span
 const remainingGuessesSpan = document.querySelector(".remaining span");
-// where guessed letters will appear
 const message = document.querySelector(".message");
-// hidden button prompting player to play again
 const playAgainButton = document.querySelector(".play-again");
 
 let word = "magnolia";
